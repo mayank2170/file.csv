00fd121895b9f462993ceabdeefe21209a74a897
mayank2170 <mayanks1312@gmail.com>
2025-08-06 14:55:15 +0530

    file upload

---
+++ file load/file-load-excel.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import openpyxl
+
+data = pd.read_excel('large_customer_data.xlsx')
+
+print(data)
---
+++ file load/file-load.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data = pd.read_csv('large_customer_data.csv')
+
+print(data)
